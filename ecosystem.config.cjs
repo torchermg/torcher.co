07f708db407a84a8456020c3c4da941fde736631
Evan Goode <mail@evangoo.de>
2021-01-31 19:15:31 -0500

    scp .env in ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -19,7 +19,7 @@ module.exports = {
 			ref  : process.env.DEPLOY_REF,
 			repo : process.env.DEPLOY_REPO,
 			path : process.env.DEPLOY_PATH,
-			"pre-deploy-local": "",
+			"pre-deploy-local": `scp .env ${process.env.DEPLOY_USER}@${process.env.DEPLOY_HOST}:${process.env.DEPLOY_PATH}/current`,
 			"post-deploy" : "make && pm2 reload ecosystem.config.cjs && pm2 save",
 			"pre-setup": "",
 		}
